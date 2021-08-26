c26ad91b97ef04ed9ce7b6038775eb0be7b88ef6
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-08-26 18:56:29 +0800

    post to pre

--- codeforces/A/71A.c
+++ codeforces/A/71A.c
@@ -9,7 +9,7 @@ int main(){
     char s[100];
  
     scanf("%d", &n);
-    while(n--){
+    while(--n){
         scanf("%s", s);
  
         if(strlen(s) > 10){
@@ -28,7 +28,7 @@ int main(){
 //     char str[100][100];
     
 //     scanf("%d", &n);
-//     for(int i =0; i < n; i++){
+//     for(int i = 0; i < n; ++i){
 //         scanf("%s", str[i]);
         
 //         if(strlen(str[i]) > 10){
